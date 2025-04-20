50c6b25908fde7629c58f947a88ad30801cac53c
gdabo1 <2214159971@qq.com>
2025-04-20 22:14:20 +0800

    改布局

--- vue/src/views/manager.vue
+++ vue/src/views/manager.vue
@@ -11,6 +11,32 @@
       <div></div>
     </div>
     <!--头部区域结束-->
+
+    <!--下方区域开始-->
+    <div style="display: flex">
+      <!--菜单区域开始-->
+      <div style="width:240px">
+        <el-menu :default-openeds="['1']" default-active="/manager/home" style="min-height: calc(100vh-60px">
+          <el-menu-item index="/manager/home">
+            <el-icon><House /></el-icon>
+            <span>首页</span>
+          </el-menu-item>
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>数据管理</span>
+            </template>
+            <el-menu-item index="1-1">二级菜单</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </div>
+      <!--菜单区域结束-->
+
+      <!--数据渲染区域开始-->
+      <div></div>
+      <!--数据渲染区域结束-->
+    </div>
+    <!--下方区域结束-->
   </div>
 </template>
 
