74b79b6f2e61d86172b00957b72a73b054e89148
hemeiyou <488412075@qq.com>
2025-04-29 22:20:05 +0800

    数据库

--- springboot/src/main/java/com/example/service/AdminService.java
+++ springboot/src/main/java/com/example/service/AdminService.java
@@ -1,15 +1,25 @@
 package com.example.service;
 
+import com.example.entity.Admin;
 import com.example.exception.CustomerException;
+import com.example.mapper.AdminMapper;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
+    @Resource
+    AdminMapper adminMapper;
     public String admin(String username) {
         if (username.equals("admin") ) {
             return "admin";
         }else {
             throw new CustomerException("账号错误");
         }
+    }
+    public List<Admin> selectAll(){
+        return adminMapper.selectAll();
     }
 }
